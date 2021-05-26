7b9974489f18c2b6918862cf25437f2f96f3d757
ayuhere <ayuhere@163.com>
2021-05-26 13:02:24 +0800

    a simple project

---
+++ build/CMakeFiles/CXXPrimer.dir/cmake_clean.cmake
@@ -0,0 +1,10 @@
+file(REMOVE_RECURSE
+  "CMakeFiles/CXXPrimer.dir/src/main.cpp.o"
+  "CXXPrimer"
+  "CXXPrimer.pdb"
+)
+
+# Per-language clean rules from dependency scanning.
+foreach(lang CXX)
+  include(CMakeFiles/CXXPrimer.dir/cmake_clean_${lang}.cmake OPTIONAL)
+endforeach()
